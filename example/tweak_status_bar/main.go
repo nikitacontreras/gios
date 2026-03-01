5e2fafc0d394861b118004fd9201dd67fc560865
Nicolás Contreras <nicolas.contreras@connectiva.com.co>
2026-02-28 20:17:37 -0500

    feat(example): implement heartbeat logic for status bar tweak

--- example/tweak_status_bar/main.go
+++ example/tweak_status_bar/main.go
@@ -1,30 +1,46 @@
 package main
 
 /*
-extern void TweakEntry();
+#cgo LDFLAGS: -framework Foundation -framework UIKit
+#include <Foundation/Foundation.h>
+#include <UIKit/UIKit.h>
+#include <stdio.h>
 
-__attribute__((constructor)) static void init_tweak() {
-    TweakEntry();
+// Constructor that runs when the dylib is injected into SpringBoard
+__attribute__((constructor))
+static void init_tweak() {
+    printf("[GIOS] Status Bar Tweak loaded inside SpringBoard\n");
 }
 */
 import "C"
-
 import (
 	"fmt"
 	"time"
 )
 
+// TweakEntry is protected from GC by cgo.
+// It will be called if you manually invoke it from C, 
+// but for a passive "heartbeat" tweak, we use a goroutine.
+
+func main() {
+    // This part is rarely reached in a dylib unless manually called.
+}
+
 //export TweakEntry
 func TweakEntry() {
+	fmt.Println("[GIOS] TweakEntry started. Heartbeat active.")
+	
+	// Example of a background heartbeat running inside the SpringBoard process
 	go func() {
-		// Just a simple log to demonstrate it's in the process context
+		counter := 0
 		for {
-			fmt.Printf("[Gios/StatusBar] Tweak active! Heartbeat from process...\n")
-			time.Sleep(30 * time.Second)
+			fmt.Printf("[GIOS] Heartbeat #%d from SpringBoard (PID: %d)\n", counter, time.Now().Unix())
+			
+			// Here you would eventually add MSHookMessageEx calls via CGO
+			// to modify UIStatusBar methods directly.
+			
+			counter++
+			time.Sleep(10 * time.Second)
 		}
 	}()
 }
-
-func init() {}
-
-func main() {}
